660e2eeacce7d1e18bf3bdd8ebf4581a6f637b85
Biote Ngovene <bioten11@gmail.com>
2022-09-16 22:33:12 +0200

    First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { OverviewComponent } from './components/overview/overview.component';
+
+
+const routes: Routes = [
+  {path: "", redirectTo:"home", pathMatch:"full"},
+  {path: "home", component: HomeComponent},
+  {path: ":id", component: OverviewComponent},
+  {path: "**", redirectTo:"home"}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
